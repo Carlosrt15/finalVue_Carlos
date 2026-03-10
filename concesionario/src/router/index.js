0dfb06053d5c747e9adf014702331ab8876b51c4
Carlos Ródriguez Talegon <carlosrt123450@gmail.com>
2026-03-10 19:25:57 +0100

    Upadte de las vistas ya es visible el logo

--- concesionario/src/router/index.js
+++ concesionario/src/router/index.js
@@ -1,8 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+
+import InicioView from '@/views/InicioView.vue'
+import MarcasView from '@/views/MarcasView.vue'
+import NuevaMarcaView from '@/views/NuevaMarcaView.vue'
+import ModelosView from '@/views/ModelosView.vue'
+import NuevoModeloView from '@/views/NuevoModeloView.vue'
+import VehiculosView from '@/views/VehiculosView.vue'
+import NuevoVehiculoView from '@/views/NuevoVehiculoView.vue'
+import AlquilerView from '@/views/AlquilerView.vue'
+import ClientesView from '@/views/ClientesView.vue'
+
+const routes = [
+  { path: "/", component: InicioView},
+  { path: "/marcas", component: MarcasView },
+  { path: "/nueva-marca", component: NuevaMarcaView },
+  { path: "/modelos", component: ModelosView },
+  { path: "/nuevo-modelo", component: NuevoModeloView },
+  { path: "/vehiculos", component: VehiculosView },
+  { path: "/nuevo-vehiculo", component: NuevoVehiculoView },
+  { path: "/alquiler", component: AlquilerView },
+  { path: "/clientes", component: ClientesView }
+]
+
+
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes
 })
 
 export default router
---
+++ concesionario/src/views/InicioView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import logo from '../Logo.png'
+</script>
+
+
+<template>
+
+<div>
+
+<h1>Empresa Renting</h1>
+
+<img :src="logo" alt="logo">
+
+</div>
+
+</template>
